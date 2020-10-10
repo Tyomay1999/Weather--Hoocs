5c6ee5b4d7da727acd13beb6a7bbd0eb6dbe81d5
Tyomay1999 <artyom.bordulanyk@gmail.com>
2020-10-10 16:20:28 +0300

    install react

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const API_URL = 'https://api.openweathermap.org/data/2.5/;';
+const API_KEY = '40a3ab422b6c7446253471c3714edfb8';
+//`${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric`
+//Full Api ----> https://api.openweathermap.org/data/2.5/forecast?q=${CityName}&APPID=40a3ab422b6c7446253471c3714edfb8&units=metric
